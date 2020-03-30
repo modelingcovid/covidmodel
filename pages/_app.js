35858d61520bea9e60d0c229afc4c61fabc915d3
Daryl Koopersmith <koop@stripe.com>
2020-03-29 23:02:44 -0700

    Viewport tag

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ export default function MyApp({Component, pageProps}) {
     <ComponentIdProvider>
       <Component {...pageProps} />
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@500&display=swap"
           rel="stylesheet"
