64ffc549b165ce4e3c70a6cbbd2d2a89f97dc6a2
Sebastian Bensusan <sebas@stripe.com>
2020-04-12 22:46:21 -0700

    feedback form - basic styling

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,17 +1,48 @@
 import * as React from 'react';
 import css from 'styled-jsx/css';
+import {theme} from '../styles';
 
 const googleScriptURL = "https://script.google.com/macros/s/AKfycby5j8Vkhn6fuOgta11uHXDm_4ysCOA3i1JqcR_Wd592mJl-8wvJ/exec";
 
+const styles = css`
+  .form {
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    border: 2px solid var(--color-gray1);
+    min-width: 300px;
+    max-width: 500px;
+    padding: ${theme.spacing[1]} ${theme.spacing[1]};
+  }
+
+  .input {
+    margin-bottom: ${theme.spacing[1]};
+    margin-top: ${theme.spacing[0]};
+    padding: ${theme.spacing[0]};
+    border: 1px solid var(--color-gray1);
+  }
+
+  .button {
+    border: none;
+  }
+  .right {
+    text-align: end;
+  }
+`;
+
 export function FeedbackForm() {
   return (
-    <form method="post" action={googleScriptURL}>
+    <form className="form" method="post" action={googleScriptURL}>
+      <style jsx>{styles}</style>
+      <h3>Leave us feedback</h3>
       <label for="email">Email</label>
-      <input name="email" />
-      <label for="feedback_text">Feedback</label>
-      <textarea name="feedback_text" />
+      <input type="email" className="input" name="email" placeholder="Optional" />
+      <label for="feedback_text">How could this site improve?</label>
+      <textarea className="input" name="feedback_text" />
       { (typeof window !== 'undefined') ? <input type="hidden" name="url" value={window.location.pathname} /> : null}
-      <button type="submit">Send</button>
+      <div className="right">
+        <button className="button" type="submit">Send</button>
+      </div>
     </form>
   );
 }
