72ddac3535c8317c176789c63a014c388e2525c0
Sebastian Bensusan <sebas@stripe.com>
2020-04-12 23:10:46 -0700

    feedback form - place elsewhere

--- pages/state/[state].js
+++ pages/state/[state].js
@@ -127,6 +127,11 @@ export default function StatePage({data, states}) {
               <ProjectedDeaths width={width} height={height} />
               <ParameterTable />
             </Section>
+            <Section className="margin-top-3">
+              <div>
+                <FeedbackForm />
+              </div>
+            </Section>
             <Section style={{marginTop: '300px'}}>
               <Title>
                 Work in progress: these sections are being converted to match
@@ -138,7 +143,6 @@ export default function StatePage({data, states}) {
           </div>
         </NearestDataProvider>
       </ModelDataProvider>
-      <FeedbackForm />
     </Layout>
   );
 }
