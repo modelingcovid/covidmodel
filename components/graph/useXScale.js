43d79a6e289d6ddc7bccde095c25110256dd5b57
Daryl Koopersmith <koop@stripe.com>
2020-05-05 03:50:02 -0700

    Fix partial xScales clipping data

--- components/graph/useXScale.js
+++ components/graph/useXScale.js
@@ -7,22 +7,30 @@ const {createContext, useCallback, useContext} = React;
 const defaultStart = new Date('2020-01-01').getTime();
 const defaultEnd = new Date('2021-01-01').getTime();
 
-export function createXScale({start, end}) {
+export function createXScale({start, end, clamp = false}) {
   return scaleUtc({
-    clamp: true,
+    clamp,
     domain: [start, end],
     range: [0, 1],
   });
 }
 
-const defaultXScale = createXScale({start: defaultStart, end: defaultEnd});
+const defaultXScale = createXScale({
+  start: defaultStart,
+  end: defaultEnd,
+  clamp: true,
+});
 const defaultXScaleFn = () => defaultXScale;
 
 export const XScaleContext = createContext(defaultXScaleFn);
 
-export function useCreateXScale({start = defaultStart, end = defaultEnd} = {}) {
+export function useCreateXScale({
+  start = defaultStart,
+  end = defaultEnd,
+  clamp,
+} = {}) {
   return useCallback(
-    memo(() => createXScale({start: maybe(start), end: maybe(end)})),
+    memo(() => createXScale({start: maybe(start), end: maybe(end), clamp})),
     [start, end]
   );
 }
