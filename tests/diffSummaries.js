f750196d9064709b76c6badaaaa14661fecec182
Will Bunting <wbunting@stripe.com>
2020-05-07 16:53:49 +0900

    fix multi row

--- tests/diffSummaries.js
+++ tests/diffSummaries.js
@@ -94,14 +94,16 @@ const main = async () => {
     const resp = await fetch(COMMENT_API_PATH, {
       method: 'POST',
       body: JSON.stringify({
-        body: `Differences in summary metrics of greater than 5% detected between runs on August 1st:\n| State | Scenario | Metric | Current | Previous | Difference | Difference Percentage |\n| --- | --- | --- | --- | --- | --- | --- | \n${differencesAboveThreshold.map(
-          (diff) =>
-            `|${diff.state}|${diff.scenario}|${diff.metric}|${formatNumber2(
-              diff.current
-            )}|${formatNumber2(diff.previous)}|${formatNumber2(
-              diff.difference
-            )}|${formatPercent2(diff.differencePercentage)}|\n`
-        )}`,
+        body: `Differences in summary metrics of greater than 5% detected between runs on August 1st:\n| State | Scenario | Metric | Current | Previous | Difference | Difference Percentage |\n| --- | --- | --- | --- | --- | --- | --- | \n${differencesAboveThreshold
+          .map(
+            (diff) =>
+              `|${diff.state}|${diff.scenario}|${diff.metric}|${formatNumber2(
+                diff.current
+              )}|${formatNumber2(diff.previous)}|${formatNumber2(
+                diff.difference
+              )}|${formatPercent2(diff.differencePercentage)}|`
+          )
+          .join('\n')}`,
       }),
       headers: {
         'Content-Type': 'application/json',
